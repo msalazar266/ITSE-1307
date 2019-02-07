10be56b2b3f5600890b6891e575c4fa0c2e788f1
msalazar266 <47163797+msalazar266@users.noreply.github.com>
2019-02-07 00:06:17 -0600

    Homework assignment
    Business card assignment.

---
+++ MRSbusinesscard/MRSbusinesscard/MRSbusinesscard.cpp
@@ -0,0 +1,45 @@
+// MRSbusinesscard.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/*
+Matthew Salazar
+ITSE-1307 Spring 2019
+20190207
+	
+	This program is designed to display a business card.
+	
+	Pseudo Code:
+		Display card.
+	*/
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//This shows a business card with contact information
+	cout << "***********************************************" << endl;
+	cout << "* \t\t MuttDogg Games\t\t      *" << endl; //Displays business name
+	cout << "* \t\t\t\t\t      *" << endl;
+	
+	cout << "* Matthew Salazar\t\t\t      *" << endl; //Displays my name
+	cout << "* Programmer\t\t\t\t      *" << endl; //Displays my title
+	cout << "* \t\t\t\t\t      *" << endl;
+	
+	cout << "* 617-369-3266 \t\t\t\t      *" << endl; //Displays phone number
+	cout << "* msalazar@muttdogggames.com\t\t      *" << endl; //Displays email address
+	cout << "***********************************************" << endl;
+	
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
