2543c7787935a8c19e783ef827a77bcdeb93eb14
msalazar266 <47163797+msalazar266@users.noreply.github.com>
2019-02-11 18:19:49 -0600

    Conversion Homework Assignment
    Convert miles to kilometers.

--- MRSconversion/MRSconversion/MRSconversion.cpp
+++ MRSconversion/MRSconversion/MRSconversion.cpp
@@ -5,12 +5,12 @@ Matthew Salazar
 ITSE-1307 Spring 2019
 20190212
 
-	  This program will convert kilometers to miles.
+	  This program will convert miles to kilometers.
 
 	  Pseudo Code:
-			   1. Ask user for kilometer distance.
-			   2. miles = kilos * 0.6215;
-			   3. Display answer in miles.
+			   1. Ask user for miles distance.
+			   2. kilo = mile * 1.609;
+			   3. Display answer in kilometers.
 
 */
 
@@ -20,20 +20,20 @@ using namespace std;
 
 int main()
 {
-	double dblKilo = 0;
 	double dblMile = 0;
+	double dblKilo = 0;
 
-	//This program will convert kilometers to miles
-	cout << "This program will convert kilometers to miles." << endl;
+	//This program will convert miles to kilometers
+	cout << "This program will convert miles to kilometers." << endl;
 	cout << "" << endl; //Empty line
 
-	cout << "Please enter the distance in kilometers: " << endl;
-	cin >> dblKilo; //User inputs a distance in kilometers
+	cout << "Please enter the distance in miles: " << endl;
+	cin >> dblMile; //User inputs a distance in miles
 
-	dblMile = dblKilo * 0.6215; //Conversion math
+	dblKilo = dblMile * 1.609; //Conversion math
 	cout << "" << endl; //Empty line
 
-	cout << dblKilo << " kilometer(s) = " << dblMile << " mile(s)." << endl; //Answer in miles
+	cout << dblMile << " miles(s) = " << dblKilo << " kilometer(s)." << endl; //Answer in kilometers
 
 	return 0;
 
