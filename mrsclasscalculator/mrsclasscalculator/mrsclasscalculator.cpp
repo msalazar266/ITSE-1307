da18ab7aeb371eb8deb01f76839dc42092470a7e
msalazar266 <47163797+msalazar266@users.noreply.github.com>
2019-02-21 19:07:50 -0600

    In class calculator

--- mrsclasscalculator/mrsclasscalculator/mrsclasscalculator.cpp
+++ mrsclasscalculator/mrsclasscalculator/mrsclasscalculator.cpp
@@ -12,13 +12,14 @@ ITSE-1307 Spring 2019
 			   2. double B = 0;
 			   3. user input A
 			   4. user input B
-			   %. C = ((A * B) / A) - B; 
+			   5. C = ((A * B) / A) - B;
 			
 */
 
 
 #include "pch.h"
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -27,17 +28,18 @@ int main()
 	double dblB = 0;
 	double dblC = 0;
 
-	cout << "This program will calculate c = ab / a - b from two user inputs" << endl;
+	//This program will calculate c = ab / a - b from two user inputs.
+	cout << "This program will calculate c = ab / a - b from two user inputs." << endl;
 
-	cout << "Please enter the first number: " << endl;
+	cout << "Please enter the first decimal number for A: " << endl;	//First decimal number input.
 	cin >> dblA;
 
-	cout << "Please enter the second number: " << endl;
+	cout << "Please enter the second decimal number for B: " << endl;	//Second decimal number input.
 	cin >> dblB;
 
-	dblC = ((dblA * dblB) / dblA) - dblB;
+	dblC = ((dblA * dblB) / dblA) - dblB;	//Math equation C = ((A * B) / A) - B.
 
-	cout << "Your answer is: " << dblC;
+	cout << "Your answer C is: " << setprecision(3) << dblC;	//Your answer displayed.
 
 	return 0;
 
